7ba6b3cffcb1215cfbd1a1d1451d17470ec74526
jihaowen11 <444043691@qq.com>
2019-04-12 17:24:21 +0800

    MY

--- src/fullCalendar.vue
+++ src/fullCalendar.vue
@@ -38,7 +38,7 @@
             <div class="events-day" v-for="day in week" track-by="$index"
                  :class="{'today' : day.isToday,
               'not-cur-month' : !day.isCurMonth}" @click.stop="dayClick(day.date, $event)">
-              <p class="day-number">{{day.monthDay}}</p>
+              <p class="day-number" :class="{ 'has-event' : event.cssClass }">{{day.monthDay}}</p>
               <div class="event-box">
                 <event-card :event="event" :date="day.date" :firstDay="firstDay" v-for="event in day.events" v-show="event.cellIndex <= eventLimit" @click="eventClick">
                   <template scope="p">
